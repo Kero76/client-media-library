e4a85839b0b34a01d43310853fc5a341eb18d097
Kero76 <nic.gille@gmail.com>
2017-07-13 16:32:50 +0200

    :art: Update routes to return all media in separated array. Update home view.

--- app/routes.php
+++ app/routes.php
@@ -16,28 +16,103 @@
  * along with Media-Library. If not, see <http://www.gnu.org/licenses/>.
  */
 declare(strict_types=1);
-
-// Home page.
-    use MediaClient\Model\Video\Anime;
     
-    $app->get('/', function() use ($app) {
+    use MediaClient\Model\Book\Comic;
+    use MediaClient\Model\Game\VideoGame;
+    use MediaClient\Model\Music\Album;
+    use MediaClient\Model\Video\Anime;
+    use MediaClient\Model\Video\Cartoon;
+    use MediaClient\Model\Video\Movie;
+    use MediaClient\Model\Video\Series;
+
+// Home page : "/".
+$app->get('/', function() use ($app) {
     // Instantiate uri, medias array's and count element on uri array.
-    $uri     = array('animes/', /*'cartoons/', 'movies/', 'series/', 'musics/', 'books/', 'comics/', 'video-games/'*/);
-    $media  = array();
+    $uri      = array('animes/', 'cartoons/', 'movies/', 'series/', 'musics/', /*'books/', 'comics/', 'video-games/'*/);
     $uri_size = count($uri);
+    $animes   = array();
+    $cartoons = array();
+    $movies   = array();
+    $series   = array();
+    $musics   = array();
+    $books    = array();
+    $comics   = array();
+    $video_games = array();
     
     // Loop on each uri and get all medias.
     for ($i = 0; $i < $uri_size; ++$i) {
         $media_request = $app['rest']->get($uri[$i]); // Get all specific media.
-        $media_object = array();
-        foreach ($media_request as $k) {
-            $id = $k['id'];
-            $media_object[$id] = new Anime($k);
+                
+        // Switch to generate right object.
+        switch ($i) {
+            // Anime
+            case 0 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $animes[$id] = new Anime($k);
+                }
+                break;
+            // Cartoons
+            case 1 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $cartoons[$id] = new Cartoon($k);
+                }
+                break;
+            // Movie
+            case 2 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $movies[$id] = new Movie($k);
+                }
+                break;
+            // Series
+            case 3 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $series[$id] = new Series($k);
+                }
+                break;
+            // Music
+            case 4 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $musics[$id] = new Album($k);
+                }
+                break;
+            // Book
+            case 5 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $books[$id] = new Book($k);
+                }
+                break;
+            // Comic
+            case 6 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $comics[$id] = new Comic($k);
+                }
+                break;
+            // VideoGame
+            case 7 :
+                foreach ($media_request as $k) {
+                    $id = $k['id'];
+                    $video_games[$id] = new VideoGame($k);
+                }
+                break;
         }
-            
-        $media = array_merge($media, $media_object);
     }
     
     // Return all medias.
-    return $app['twig']->render('home.html.twig', array('medias' => $media));
+    return $app['twig']->render('home.html.twig', array(
+        'animes'   => array_slice($animes, count($animes) - 10),
+        'cartoons' => array_slice($cartoons, count($cartoons) - 10),
+        'movies'   => array_slice($movies, count($movies) - 10),
+        'series'   => array_slice($series, count($series) - 10),
+        'musics'   => array_slice($musics, count($musics) - 10),
+        'books'    => array_slice($books, count($books) - 10),
+        'comics'   => array_slice($comics, count($comics) - 10),
+        'video_games' => array_slice($video_games, count($video_games) - 10),
+    ));
 });
